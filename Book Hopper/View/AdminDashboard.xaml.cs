8b9a77cf004a9f44d83c0214a448d17994f97340
JM <200871010@yoobeestudent.ac.nz>
2023-11-24 03:21:03 +1300

    Added needed Images to WPF windows

---
+++ Book Hopper/View/AdminDashboard.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookHopperApp.View
+{
+    /// <summary>
+    /// Interaction logic for AdminDashboard.xaml
+    /// </summary>
+    public partial class AdminDashboard : Window
+    {
+        public AdminDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Click_AdminBooks(object sender, RoutedEventArgs e)
+        {
+            AdminBooks ab = new AdminBooks();
+            ab.Show();
+        }
+
+        private void Click_Home(object sender, RoutedEventArgs e)
+        {
+           /* AdminDashboard ad = new AdminDashboard();
+            ad.Show(); */
+        }
+
+        private void Click_Members(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Book Hopper/View/UserLogin.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookHopperApp.View
+{
+    /// <summary>
+    /// Interaction logic for UserLogin.xaml
+    /// </summary>
+    public partial class UserLogin : Window
+    {
+        public UserLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AdminLoginClick(object sender, RoutedEventArgs e)
+        {
+            AdminDashboard ad = new AdminDashboard();
+            ad.Show();
+        }
+    }
+}
